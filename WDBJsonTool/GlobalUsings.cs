1288bea8ea90af942249e79aa394dc26dcb2ba85
Hector R. <github.hector1@gmail.com>
2025-12-07 22:35:23 -0600

    initial work

---
+++ WDBJsonTool/GlobalUsings.cs
@@ -0,0 +1,8 @@
+// Global using directives for .NET 9.0 C# 13
+global using System;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using WDBJsonTool.Core.Models;
+global using WDBJsonTool.Core.Enums;
+global using WDBJsonTool.Core.Exceptions;
